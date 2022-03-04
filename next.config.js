ea6dbd09b3d0dcf85fe66f0a8dff8f6c9208bf80
rahulyadav139 <ry.rahulmyadav@gmail.com>
2022-03-04 21:46:23 +0530

    refactor: env variables updated

--- next.config.js
+++ next.config.js
@@ -4,13 +4,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
-// env variables
-
-module.exports = {
-  env: {
-    DB_USERNAME: 'rahul',
-    DB_PASSWORD: '4obHX7fDphFFe6bG',
-    COOKIE_SECRET_KEY: 'g2(dqztb;?kadfgsu.sfsX9baKj.IK0!L,L}~',
-  },
-};
