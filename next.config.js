462c43563f7384066cc203df2b19877a0b6251e8
rahulyadav139 <ry.rahulmyadav@gmail.com>
2022-03-04 23:14:18 +0530

    fix: env variable issue

--- next.config.js
+++ next.config.js
@@ -6,11 +6,3 @@ const nextConfig = {
 module.exports = nextConfig;
 
 // env variables
-
-module.exports = {
-  env: {
-    DB_USERNAME: process.env.DB_USERNAME,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    COOKIE_SECRET_KEY: process.env.COOKIE_SECRET_KEY,
-  },
-};
